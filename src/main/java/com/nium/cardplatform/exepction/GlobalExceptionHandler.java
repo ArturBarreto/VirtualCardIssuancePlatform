a4605d04d44c56207e4cb0223ee75933bdc66427
Artur Barreto <artur.gomes.barreto@gmail.com>
2025-07-24 10:57:37 +0200

    feat: implementation of exceptions and correction of typo in package name

--- src/main/java/com/nium/cardplatform/exepction/GlobalExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.nium.cardplatform.exepction;
-
-public class GlobalExceptionHandler extends RuntimeException {
-    public GlobalExceptionHandler(String message) {
-        super(message);
-    }
-}
