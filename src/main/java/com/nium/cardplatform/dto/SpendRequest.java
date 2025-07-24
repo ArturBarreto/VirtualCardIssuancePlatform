703d10e7d412d391320834c3e8d449f01b6f98db
Artur Barreto <artur.gomes.barreto@gmail.com>
2025-07-24 10:36:10 +0200

    feat: implementation of dtos

--- src/main/java/com/nium/cardplatform/dto/SpendRequest.java
+++ src/main/java/com/nium/cardplatform/dto/SpendRequest.java
@@ -1,4 +1,17 @@
 package com.nium.cardplatform.dto;
 
+import java.math.BigDecimal;
+
 public class SpendRequest {
+    private BigDecimal amount;
+
+    // Getters and setters
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/com/nium/cardplatform/dto/TopupRequest.java
+++ src/main/java/com/nium/cardplatform/dto/TopupRequest.java
@@ -1,4 +1,17 @@
 package com.nium.cardplatform.dto;
 
+import java.math.BigDecimal;
+
 public class TopupRequest {
+    private BigDecimal amount;
+
+    // Getters and setters
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
